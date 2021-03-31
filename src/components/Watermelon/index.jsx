d668f65e31b62baf668445a9d86855000b322b03
zhaoyf <604264588@qq.com>
2021-03-31 19:36:29 +0800

    A 添加 西瓜 动画

---
+++ src/components/Watermelon/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './index.scss'
+const Watermelon = (props) => {
+
+  return (
+    <div className="watermelon">
+      <div className="deep-0"></div>
+      <div className="deep-1"></div>
+      <div className="deep-2"></div>
+      <div className="deep-3"></div>
+      <div className="dot-list">
+        <div className="wrap">
+          <div className="dot-item"></div>
+          <div className="dot-item"></div>
+          <div className="dot-item"></div>
+          <div className="dot-item"></div>
+          <div className="dot-item"></div>
+          <div className="dot-item"></div>
+          <div className="dot-item"></div>
+          <div className="dot-item"></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Watermelon
